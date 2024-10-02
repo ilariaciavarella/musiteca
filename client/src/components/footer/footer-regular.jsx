5e1726bbb1ecdc785f0540d18f4c801c43001364
Ilaria <ilariaciavarella.ic@gmail.com>
2024-10-02 18:50:32 +0200

    Replace footer containers with ant elements

--- client/src/components/footer/footer-regular.jsx
+++ client/src/components/footer/footer-regular.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { LinkedinLogo, InstagramLogo, GithubLogo } from "@phosphor-icons/react";
-import { Button } from "antd";
+import { Button, Space, Flex } from "antd";
 
 import "./footer.scss";
 
@@ -12,29 +12,35 @@ function FooterRegular(props) {
     <footer
       className={props.light ? "footer-regular footer-light" : "footer-regular"}
     >
-      <div className="credits">
-        <img src={props.light ? LogoLight : LogoDark} alt="Musiteca Logo" />
-        <small>
-          © 2024 Musiteca by <em>Ilaria Ciavarella</em> – All rights reserved
-        </small>
-      </div>
-      <div className="social-links">
-        <Button
-          href="https://www.linkedin.com/in/ilaria-ciavarella"
-          icon={<LinkedinLogo size={28} />}
-          type="link"
-        />
-        <Button
-          href="https://github.com/ilariaciavarella"
-          icon={<GithubLogo size={28} />}
-          type="link"
-        />
-        <Button
-          href="https://www.instagram.com/lil.ciavarella/"
-          icon={<InstagramLogo size={28} />}
-          type="link"
-        />
-      </div>
+      <Flex justify="space-around" align="center" gap="small" wrap>
+        <Flex justify="center" align="center" gap="large">
+          <img
+            src={props.light ? LogoLight : LogoDark}
+            alt="Musiteca Logo"
+            className="footer-logo"
+          />
+          <small>
+            © 2024 Musiteca by <em>Ilaria Ciavarella</em> – All rights reserved
+          </small>
+        </Flex>
+        <Space size="small" className="social-links">
+          <Button
+            href="https://www.linkedin.com/in/ilaria-ciavarella"
+            icon={<LinkedinLogo size={28} />}
+            type="link"
+          />
+          <Button
+            href="https://github.com/ilariaciavarella"
+            icon={<GithubLogo size={28} />}
+            type="link"
+          />
+          <Button
+            href="https://www.instagram.com/lil.ciavarella/"
+            icon={<InstagramLogo size={28} />}
+            type="link"
+          />
+        </Space>
+      </Flex>
     </footer>
   );
 }
