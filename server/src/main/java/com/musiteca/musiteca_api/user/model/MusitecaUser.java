7ae6e2f42678a13de285ea2e132e7bc5a9e2acdb
Ilaria <ilariaciavarella.ic@gmail.com>
2024-10-20 02:05:12 +0200

    Start defining user class

--- server/src/main/java/com/musiteca/musiteca_api/user/model/MusitecaUser.java
+++ server/src/main/java/com/musiteca/musiteca_api/user/model/MusitecaUser.java
@@ -1,4 +1,23 @@
 package com.musiteca.musiteca_api.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Document(collection = "users") @Data
+@NoArgsConstructor @AllArgsConstructor
 public class MusitecaUser {
+    @Id
+    private ObjectId id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private Set<Role> roles = new HashSet<>();
 }
--- server/src/main/java/com/musiteca/musiteca_api/user/model/Role.java
+++ server/src/main/java/com/musiteca/musiteca_api/user/model/Role.java
@@ -1,4 +1,17 @@
 package com.musiteca.musiteca_api.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "roles") @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+    @Id
+    private ObjectId id;
+    private String role;
 }
