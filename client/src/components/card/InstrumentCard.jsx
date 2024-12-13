f5420041a50f27f781435a5373e8590c5eaec584
Ilaria <ilariaciavarella.ic@gmail.com>
2024-12-13 22:39:12 +0100

    Fix layout issue on card

--- client/src/components/card/InstrumentCard.jsx
+++ client/src/components/card/InstrumentCard.jsx
@@ -16,43 +16,45 @@ function InstrumentCard(props) {
     <div className="card">
       <img src="https://picsum.photos/600/800" className="instrument-image" />
       <div className="post-content">
-        <div className="post-details">
-          <Avatar src={props.userPic} shape="square" size={52}>
-            {props.userName.slice(0, 1)}
-          </Avatar>
-          <div>
-            <div className="post-author">{props.userName}</div>
+        <div>
+          <div className="post-details">
+            <Avatar src={props.userPic} shape="square" size={52}>
+              {props.userName.slice(0, 1)}
+            </Avatar>
             <div>
-              {`${props.createdTime}m • `}
-              <strong>{`${props.userLocation}`}</strong>
+              <div className="post-author">{props.userName}</div>
+              <div>
+                {`${props.createdTime}m • `}
+                <strong>{`${props.userLocation}`}</strong>
+              </div>
             </div>
           </div>
+          <p>
+            Cupcake ipsum dolor sit amet chupa chups toffee donut. Gingerbread
+            gingerbread tart I love oat cake apple pie fruitcake cotton candy.
+            Chocolate bar gingerbread cake I love wafer muffin.
+          </p>
+          <Button
+            type="link"
+            className="btn-invisible-small"
+            onClick={toggleInstrumentDetails}
+          >
+            Show details
+          </Button>
+          {showInstrumentDetails && (
+            <ul>
+              <li>
+                <strong>Instrument:</strong>
+              </li>
+              <li>
+                <strong>Brand:</strong>
+              </li>
+              <li>
+                <strong>Age:</strong>
+              </li>
+            </ul>
+          )}
         </div>
-        <p>
-          Cupcake ipsum dolor sit amet chupa chups toffee donut. Gingerbread
-          gingerbread tart I love oat cake apple pie fruitcake cotton candy.
-          Chocolate bar gingerbread cake I love wafer muffin.
-        </p>
-        <Button
-          type="link"
-          className="btn-invisible-small"
-          onClick={toggleInstrumentDetails}
-        >
-          Show details
-        </Button>
-        {showInstrumentDetails && (
-          <ul>
-            <li>
-              <strong>Instrument:</strong>
-            </li>
-            <li>
-              <strong>Brand:</strong>
-            </li>
-            <li>
-              <strong>Age:</strong>
-            </li>
-          </ul>
-        )}
         <Flex gap="middle" className="post-actions">
           <Button icon={<ShareFat size={28} />} />
           <Button
